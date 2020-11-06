49baa77375536d01f1858baad52e607b2d36de64
hamihami <rinrin.xml@gmail.com>
2020-11-06 19:56:00 +0000

    백준 for문 풀이

--- baekjoon/main.py
+++ baekjoon/main.py
@@ -1,21 +1,32 @@
 from question_if import compare_two_nums, test_scores,choose_quadrant, alarm_clock
 from question_greedy import greedy_11399, greedy_11047, greedy_1541, greedy_1931
 from question_while import while_10952, while_10951, while_1110
-from question_for import for_2739, for_10950, for_8393, for_15552
+from question_for import for_2739, for_10950, for_8393, for_15552, for_2741, for_2742, for_11021, for_11022, for_2438, for_2439, for_10871
+
 
 # compare_two_nums()
 # test_scores()
 # choose_quadrant()
 # alarm_clock()
 
+
 # while_10952()
 # while_10951()
 # while_1110()
 
+
 # for_2739()
 # for_10950()
 # for_8393()
-for_15552()
+# for_15552()
+# for_2741()
+# for_2742()
+# for_11021()
+# for_11022()
+# for_2438()
+# for_2439()
+#for_10871()
+
 
 # greedy_11399()
 # greedy_11047()
--- baekjoon/question_for.py
+++ baekjoon/question_for.py
@@ -32,4 +32,53 @@ def for_15552():
     a, b = map(int, sys.stdin.readline().split())
     print(a+b)
 
+# 2741 번 : N 찍기
+def for_2741():
+  n = int(input())
+  for i in range(n):
+    print(i+1)
 
+# 2742 번 : 기찍 N
+def for_2742():
+  n = int(input())
+  for i in range(n):
+    print(n-i)
+
+# 11021 번 : A + B - 7
+def for_11021():
+  n = int(input())
+  for i in range(n):
+    a, b = map(int, input().split())
+    print("Case #{0}:".format(i+1), a+b)
+
+# 11022 번 : A + B - 8
+def for_11022():
+  n = int(input())
+  for i in range(n):
+    a, b = map(int, input().split())
+    print("Case #{0}: {1} + {2} =".format(i+1, a, b), a+b)
+
+# 2438 번 : 별찍기
+def for_2438():
+  n = int(input())
+  for i in range(n):
+    print( "*"*(i+1) )
+
+
+# 2439 번 : 별찍기 -2
+def for_2439():
+  n = int(input())
+  for i in range(n):
+    print( " "*(n-i-1)+"*"*(i+1) )
+
+
+# 10871 번 : x보다 작은 수
+def for_10871():
+  n, x = map(int, input().split())
+  num_arr = list(map(int, input().split()))
+  result = ""
+
+  for i in range(n):
+    if num_arr[i] < x:
+      result += str(num_arr[i])+" "
+  print(result)
